895c736d7130d1567f054fae98e9e2628e05b379
lakshayjain99 <48042411+lakshayjain99@users.noreply.github.com>
2024-07-13 16:19:12 +0100

    Add files via upload

---
+++ static/js/popup.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() {
+    alert("Dear [Friend's Name],\n\nI created this website to express how truly sorry I am for the things I've said. I hope you can find it in your heart to forgive me.\n\nClick OK to proceed.");
+});
